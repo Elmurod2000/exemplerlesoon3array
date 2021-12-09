ffa59424eb82f254e17763b938bc733decdc171f
Elmurod2000 <elmurodburiboev@gmail.com>
2021-12-09 17:34:13 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[][] numbers = new int [3][4];
+        System.out.println(Arrays.deepToString(numbers));
+        numbers [1] [3]=9;
+        System.out.println(Arrays.deepToString(numbers));
+        for (int i = 0; i < ; i++) {
+            
+        }
+                
+    }
+}
